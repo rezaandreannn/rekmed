1552a35cb80a39fdf763d1f260266f59f9f536cd
Reza andrean <72512570+rezaandreannn@users.noreply.github.com>
2022-07-09 16:53:57 +0700

    ubah title login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout title="Login">
+<x-guest-layout title="Login page">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
